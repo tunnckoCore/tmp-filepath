c0a733f53b7639573879549cce14edbd9b9bb524
tunnckoCore <mameto_100@mail.bg>
2017-01-03 02:11:42 +0200

    docs(api): run verb, generate api docs

--- index.js
+++ index.js
@@ -11,6 +11,27 @@ const os = require('os')
 const path = require('path')
 const cuid = require('cuid')
 
+/**
+ * > Generate temporary filepath string.
+ *
+ * **Example**
+ *
+ * ```js
+ * const tmpFilepath = require('tmp-filepath')
+ *
+ * console.log(tmpFilepath())
+ * // => /tmp/cia0ervrm0000vppvw0t61v9m
+ *
+ * console.log(tmpFilepath('.js'))
+ * // => /tmp/cia0ervrm0000vppvw0t61v9m.js
+ * ```
+ *
+ * @name   tmpFilepath
+ * @param  {String} `[ext]` extension to be appended to generated filepath
+ * @return {String} generated filepath
+ * @api public
+ */
+
 const tmpFilepath = (ext) => path.join(os.tmpdir(), cuid() + (ext || ''))
 
 module.exports = exports.default = tmpFilepath
